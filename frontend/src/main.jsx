e4ef688c02495ef43e18dd71438bed669c105ac4
Jason Dinh <ndin0017@student.monash.edu>
2026-08-14 00:11:52 +1000

    Create a password page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
+import PasswordGate from './features/password/Password.Gate.jsx'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <PasswordGate>
     <App />
-  </StrictMode>,
+  </PasswordGate>,
 )
